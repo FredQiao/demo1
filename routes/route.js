4a7c509a193ad295643a4ababd3c0ae3fb1b856c
FredQiao <ecit_yfqiao@hotmail.com>
2017-12-01 10:14:34 +0800

    add form.ejs

--- routes/route.js
+++ routes/route.js
@@ -14,20 +14,16 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
-    console.log("go user");
-    request('http://localhost:9090/api/hello/1312', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body) // Show the HTML for the baidu homepage.
-            res.render('index', { title: 'Express', name: body });
-        }
-    })
+router.get('/form', function(req, res, next) {
+    console.log("go form");
+    res.render('form', { title: 'Express' });
 });
 
-/* GET users listing. */
-router.get('/air', function(req, res, next) {
-    console.log("go air");
-    res.render('index', { title: 'Express', name: "air" });
+/* upload file */
+router.post('/upload', function(req, res, next) {
+    var file = req.body.file;
+    console.log("go air" + file);
+    res.render('index', { title: 'Express', name: file });
 });
 
 module.exports = router;
